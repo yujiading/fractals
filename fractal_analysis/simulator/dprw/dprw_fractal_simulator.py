bc86ef588a44d0df034207ae80aac06eff4a0f06
yujiading <yujia.ding@cgu.edu>
2025-04-23 15:19:22 -0700

    apply high-precision computation logic reusably across multiple covariance functions. apply high-precision computation for bi-fbm simulation.

--- fractal_analysis/simulator/dprw/dprw_fractal_simulator.py
+++ fractal_analysis/simulator/dprw/dprw_fractal_simulator.py
@@ -1,7 +1,9 @@
+from decimal import Decimal, getcontext
 from typing import Callable
 
 import numpy as np
 import scipy
+
 from fractal_analysis.simulator.wood_chan.wood_chan_fractal_simulator import WoodChanFgnSimulator
 
 
@@ -18,13 +20,13 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
     """
 
     def __init__(self, sample_size: int, hurst_parameter: float, covariance_func: Callable,
-                 lamperti_multiplier: int = 5,
-                 tmax: float = 1, std_const: float = 1):
+                 lamperti_multiplier: int = 5, factor: float = None, tmax: float = 1, std_const: float = 1):
         """
             sample_size: is the length of samples generated; it is a positive integer.
             hurst_parameter: is a real value in (0:1) that governs both the pointwise regularity and the shape around 0 of the power spectrum.
-            covariance_func: covariance function of the self similar process
+            covariance_func: covariance function of the self similar process (elementwise)
             lamperti_multiplier: an integer used for Lamperti transform; bigger value (usually <=10) provides more accuracy; default value is 5
+            factor: eg sub/bi/tri factor
             tmax: generates a self similar process using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates a self similar process using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
@@ -35,6 +37,7 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
             raise ValueError(f'lamperti_multiplier must be a positive integer.')
         self.lamperti_multiplier = lamperti_multiplier
         self.lamperti_series_len = self.lamperti_multiplier * self.sample_size
+        self.factor = factor
 
     @property
     def _lamperti_subseq_index(self):
@@ -50,13 +53,48 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
     def get_self_similar_process(self, is_plot=False, method_name=None, series_name=None, seed=None,
                                  plot_path: str = None, y_limits: list = None):
         series_t, lamperti_subseq_index = self._lamperti_subseq_index
-        lamp_fgn = self.get_fgn(seed=seed, N=self.lamperti_series_len, cov=self.covariance_func)
+        lamp_fgn = self.get_fgn(seed=seed, N=self.lamperti_series_len, cov=self.covariance_line)
         lamp_fgn = lamp_fgn - lamp_fgn[0]
         self_similar = series_t ** self.hurst_parameter * lamp_fgn[lamperti_subseq_index]
         if is_plot:
             self.plot(series=self_similar, method_name=method_name, series_name=series_name, save_path=plot_path,
                       y_limits=y_limits)
         return self_similar
+
+    def covariance_with_adaptive_precision(self, k, n, hurst, factor, tolerance=0.0001, initial_prec=17, step=5,
+                                           max_prec=1000000):
+        """
+        Repeatedly calls covariance func with increasing precision until the result stabilizes.
+        covariance func should return a Decimal.
+        """
+        getcontext().prec = initial_prec
+
+        n_de = Decimal(str(n))
+        hurst_de = Decimal(str(hurst))
+        k_de = Decimal(str(k))
+        if factor is not None:
+            factor_de = Decimal(str(factor))
+        else:
+            factor_de = None
+
+        tolerance = Decimal(str(tolerance))
+        v_prev = Decimal('0')
+        v = self.covariance_func(k_de=k_de, n_de=n_de, hurst_de=hurst_de, factor_de=factor_de)
+
+        while (v == 0 or abs(v - v_prev) >= tolerance) and getcontext().prec < max_prec:
+            v_prev = v
+            getcontext().prec += step
+            v = self.covariance_func(k_de=k_de, n_de=n_de, hurst_de=hurst_de, factor_de=factor_de)
+
+        return float(v)
+
+    def covariance_line(self, k):
+        v = np.array([
+            self.covariance_with_adaptive_precision(k=k_ele, n=self.sample_size, hurst=self.hurst_parameter,
+                                                    factor=self.factor) for
+            k_ele in k
+        ])
+        return v
 
 
 class DprwSubFbmSimulator(DprwSelfSimilarFractalSimulator):
@@ -71,8 +109,8 @@ class DprwSubFbmSimulator(DprwSelfSimilarFractalSimulator):
                    length (lamperti_series_len_multiplier*series_len) than the sub-FBM series.
     """
 
-    def __init__(self, sample_size: int, hurst_parameter: float,
-                 lamperti_multiplier: int = 5, tmax: float = 1, std_const: float = 1):
+    def __init__(self, sample_size: int, hurst_parameter: float, lamperti_multiplier: int = 5,
+                 tmax: float = 1, std_const: float = 1):
         """
             sample_size: is the length of samples generated; it is a positive integer.
             hurst_parameter: is a real value in (0:1) that governs both the pointwise regularity and the shape around 0 of the power spectrum.
@@ -122,19 +160,27 @@ class DprwBiFbmSimulator(DprwSelfSimilarFractalSimulator):
             tmax: generates bi-FBM using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates bi-FBM using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
-
-        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
-                         covariance_func=self.bi_fbm_covariance_func,
-                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
         self.bi_factor = bi_factor
+        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
+                         covariance_func=self.bi_fbm_covariance_func, factor=self.bi_factor,
+                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def bi_fbm_covariance_func(self, k):
-        n = self.sample_size
-        k_h_n = k * self.hurst_parameter / n
-        k_n_2 = k / n / 2
-        h2 = 2 * self.hurst_parameter
-        v = ((n ** k_h_n + n ** (-k_h_n)) ** self.bi_factor - np.abs(n ** k_n_2 - n ** (-k_n_2)) ** (
-                h2 * self.bi_factor)) / (2 ** self.bi_factor)
+    def bi_fbm_covariance_func(self, k_de, n_de, hurst_de, factor_de):
+        bi_factor_de = factor_de  # for clarity
+        k_h_n_de = k_de * hurst_de / n_de
+        k_n_2_de = k_de / n_de / 2
+        h2 = 2 * hurst_de
+
+        A = n_de ** k_h_n_de
+        a = n_de ** (-k_h_n_de)
+
+        B = n_de ** k_n_2_de
+        b = n_de ** (-k_n_2_de)
+
+        term1 = (A + a) ** bi_factor_de
+        term2 = abs(B - b) ** (h2 * bi_factor_de)
+        v = (term1 - term2)/(2 ** bi_factor_de)
+
         return v
 
     def get_bi_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
@@ -166,11 +212,10 @@ class DprwTriFbmSimulator(DprwSelfSimilarFractalSimulator):
             tmax: generates tri-FBM using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates tri-FBM using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
-
-        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
-                         covariance_func=self.tri_fbm_covariance_func,
-                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
         self.tri_factor = tri_factor
+        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
+                         covariance_func=self.tri_fbm_covariance_func, factor=self.tri_factor,
+                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
     def tri_fbm_covariance_func(self, k):
         n = self.sample_size
