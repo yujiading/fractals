Bugfix/precision limits (#7)
* Speed optimization.
Add toggle for precise vs clipped eigenvalue checks.
Allow custom y-axis limits in plotting.

* Apply custom y-axis limits to all plots.

* apply high-precision computation logic reusably across multiple covariance functions.
apply high-precision computation for bi-fbm simulation.

* use mpmath instead of decimal to calculate arbitrary precision math.
apply high-precision computation for neg-fbm, tri-fbm, sub-fbm simulation.
update poetry.

* add more tests.
update readme.

fractal_analysis/simulator/dprw/dprw_fractal_simulator.py
@@ -1,7 +1,9 @@
 from typing import Callable
+from mpmath import mp, mpf, gamma, sin, hyp2f1
 
 import numpy as np
 import scipy
+
 from fractal_analysis.simulator.wood_chan.wood_chan_fractal_simulator import WoodChanFgnSimulator
 
 
@@ -18,13 +20,13 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
     """
 
     def __init__(self, sample_size: int, hurst_parameter: float, covariance_func: Callable,
-                 lamperti_multiplier: int = 5,
-                 tmax: float = 1, std_const: float = 1):
+                 lamperti_multiplier: int = 5, factor: float = None, tmax: float = 1, std_const: float = 1):
         """
             sample_size: is the length of samples generated; it is a positive integer.
             hurst_parameter: is a real value in (0:1) that governs both the pointwise regularity and the shape around 0 of the power spectrum.
-            covariance_func: covariance function of the self similar process
+            covariance_func: covariance function of the self similar process (elementwise)
             lamperti_multiplier: an integer used for Lamperti transform; bigger value (usually <=10) provides more accuracy; default value is 5
+            factor: eg sub/bi/tri factor
             tmax: generates a self similar process using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates a self similar process using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
@@ -35,6 +37,7 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
             raise ValueError(f'lamperti_multiplier must be a positive integer.')
         self.lamperti_multiplier = lamperti_multiplier
         self.lamperti_series_len = self.lamperti_multiplier * self.sample_size
+        self.factor = factor
 
     @property
     def _lamperti_subseq_index(self):
@@ -48,14 +51,47 @@ class DprwSelfSimilarFractalSimulator(WoodChanFgnSimulator):
         return series_t, lamperti_subseq_index.astype(int)
 
     def get_self_similar_process(self, is_plot=False, method_name=None, series_name=None, seed=None,
-                                 plot_path: str = None):
+                                 plot_path: str = None, y_limits: list = None):
         series_t, lamperti_subseq_index = self._lamperti_subseq_index
-        lamp_fgn = self.get_fgn(seed=seed, N=self.lamperti_series_len, cov=self.covariance_func)
+        lamp_fgn = self.get_fgn(seed=seed, N=self.lamperti_series_len, cov=self.covariance_line)
         lamp_fgn = lamp_fgn - lamp_fgn[0]
         self_similar = series_t ** self.hurst_parameter * lamp_fgn[lamperti_subseq_index]
         if is_plot:
-            self.plot(series=self_similar, method_name=method_name, series_name=series_name, save_path=plot_path)
+            self.plot(series=self_similar, method_name=method_name, series_name=series_name, save_path=plot_path,
+                      y_limits=y_limits)
         return self_similar
+
+    def covariance_with_adaptive_precision(self, k_de, n_de, hurst_de, factor_de, tolerance=0.0001, initial_prec=17,
+                                           step=5, max_prec=1000000):
+        """
+        Repeatedly calls covariance func with increasing precision until the result stabilizes.
+        covariance func should return a Decimal.
+        """
+        mp.dps = initial_prec
+
+        tolerance = mpf(str(tolerance))
+        v_prev = mpf('0')
+        v = self.covariance_func(k_de=k_de, n_de=n_de, hurst_de=hurst_de, factor_de=factor_de)
+
+        while (v <= 0 or abs(v - v_prev) >= tolerance) and mp.dps < max_prec:
+            v_prev = v
+            mp.dps += step
+            v = self.covariance_func(k_de=k_de, n_de=n_de, hurst_de=hurst_de, factor_de=factor_de)
+
+        return float(v)
+
+    def covariance_line(self, k):
+        n_de = mpf(str(self.sample_size))
+        hurst_de = mpf(str(self.hurst_parameter))
+        if self.factor is not None:
+            factor_de = mpf(str(self.factor))
+        else:
+            factor_de = None
+        v = np.array([
+            self.covariance_with_adaptive_precision(k_de=mpf(str(k_ele)), n_de=n_de, hurst_de=hurst_de,
+                                                    factor_de=factor_de) for
+            k_ele in k])
+        return v
 
 
 class DprwSubFbmSimulator(DprwSelfSimilarFractalSimulator):
@@ -70,8 +106,8 @@ class DprwSubFbmSimulator(DprwSelfSimilarFractalSimulator):
                    length (lamperti_series_len_multiplier*series_len) than the sub-FBM series.
     """
 
-    def __init__(self, sample_size: int, hurst_parameter: float,
-                 lamperti_multiplier: int = 5, tmax: float = 1, std_const: float = 1):
+    def __init__(self, sample_size: int, hurst_parameter: float, lamperti_multiplier: int = 5,
+                 tmax: float = 1, std_const: float = 1):
         """
             sample_size: is the length of samples generated; it is a positive integer.
             hurst_parameter: is a real value in (0:1) that governs both the pointwise regularity and the shape around 0 of the power spectrum.
@@ -84,18 +120,18 @@ class DprwSubFbmSimulator(DprwSelfSimilarFractalSimulator):
                          covariance_func=self.sub_fbm_covariance_func,
                          lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def sub_fbm_covariance_func(self, k):
-        h2 = 2 * self.hurst_parameter
-        n = self.sample_size
-        k_h_n = k * self.hurst_parameter / n
-        k_n_2 = k / n / 2
-        v = n ** k_h_n + n ** (-k_h_n) - 0.5 * (
-                (n ** k_n_2 + n ** (-k_n_2)) ** h2 + np.abs(n ** k_n_2 - n ** (-k_n_2)) ** h2)
+    def sub_fbm_covariance_func(self, k_de, n_de, hurst_de, factor_de):
+        h2 = 2 * hurst_de
+        n = n_de
+        k_h_n = k_de * hurst_de / n_de
+        k_n_2 = k_de / n_de / 2
+        v = n ** k_h_n + n ** (-k_h_n) - (
+                (n ** k_n_2 + n ** (-k_n_2)) ** h2 + abs(n ** k_n_2 - n ** (-k_n_2)) ** h2) / 2
         return v
 
-    def get_sub_fbm(self, is_plot=False, seed=None, plot_path: str = None):
+    def get_sub_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
         sub_fbm = self.get_self_similar_process(is_plot=is_plot, seed=seed, method_name='DPRW', series_name='Sub-FBM',
-                                                plot_path=plot_path)
+                                                plot_path=plot_path, y_limits=y_limits)
         return sub_fbm
 
 
@@ -121,24 +157,33 @@ class DprwBiFbmSimulator(DprwSelfSimilarFractalSimulator):
             tmax: generates bi-FBM using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates bi-FBM using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
-
-        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
-                         covariance_func=self.bi_fbm_covariance_func,
-                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
         self.bi_factor = bi_factor
+        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
+                         covariance_func=self.bi_fbm_covariance_func, factor=self.bi_factor,
+                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def bi_fbm_covariance_func(self, k):
-        n = self.sample_size
-        k_h_n = k * self.hurst_parameter / n
-        k_n_2 = k / n / 2
-        h2 = 2 * self.hurst_parameter
-        v = ((n ** k_h_n + n ** (-k_h_n)) ** self.bi_factor - np.abs(n ** k_n_2 - n ** (-k_n_2)) ** (
-                h2 * self.bi_factor)) / (2 ** self.bi_factor)
+    def bi_fbm_covariance_func(self, k_de, n_de, hurst_de, factor_de):
+        bi_factor_de = factor_de  # for clarity
+        k_h_n_de = k_de * hurst_de / n_de
+        k_n_2_de = k_de / n_de / 2
+        h2 = 2 * hurst_de
+
+        A = n_de ** k_h_n_de
+        a = n_de ** (-k_h_n_de)
+
+        B = n_de ** k_n_2_de
+        b = n_de ** (-k_n_2_de)
+
+        term1 = (A + a) ** bi_factor_de
+        term2 = abs(B - b) ** (h2 * bi_factor_de)
+        v = (term1 - term2) / (2 ** bi_factor_de)
+
         return v
 
-    def get_bi_fbm(self, is_plot=False, seed=None, plot_path: str = None):
+    def get_bi_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
         bi_fbm = self.get_self_similar_process(is_plot=is_plot, seed=seed, method_name='DPRW',
-                                               series_name=f'{self.bi_factor} Bi-FBM', plot_path=plot_path)
+                                               series_name=f'{self.bi_factor} Bi-FBM', plot_path=plot_path,
+                                               y_limits=y_limits)
         return bi_fbm
 
 
@@ -164,24 +209,25 @@ class DprwTriFbmSimulator(DprwSelfSimilarFractalSimulator):
             tmax: generates tri-FBM using a specific size of time support, i.e. the time runs in [0,tmax]; if tmax is not specified, the default value is tmax = 1.
             std_const: generates tri-FBM using a specific standard deviation at instant t = 1; if std_const is not specified, the default value is std_const = 1.
         """
-
-        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
-                         covariance_func=self.tri_fbm_covariance_func,
-                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
         self.tri_factor = tri_factor
+        super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter,
+                         covariance_func=self.tri_fbm_covariance_func, factor=self.tri_factor,
+                         lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def tri_fbm_covariance_func(self, k):
-        n = self.sample_size
-        k_h_f_n = k * self.hurst_parameter * self.tri_factor / n
-        k_n_2 = k / n / 2
-        h2 = 2 * self.hurst_parameter
-        v = n ** k_h_f_n + n ** (-k_h_f_n) - np.abs(n ** k_n_2 - n ** (-k_n_2)) ** (
-                h2 * self.tri_factor)
+    def tri_fbm_covariance_func(self, k_de, n_de, hurst_de, factor_de):
+        tri_factor = factor_de
+        n = n_de
+        k_h_f_n = k_de * hurst_de * tri_factor / n_de
+        k_n_2 = k_de / n_de / 2
+        h2 = 2 * hurst_de
+        v = n ** k_h_f_n + n ** (-k_h_f_n) - abs(n ** k_n_2 - n ** (-k_n_2)) ** (
+                h2 * tri_factor)
         return v
 
-    def get_tri_fbm(self, is_plot=False, seed=None, plot_path: str = None):
+    def get_tri_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
         tri_fbm = self.get_self_similar_process(is_plot=is_plot, seed=seed, method_name='DPRW',
-                                                series_name=f'{self.tri_factor} Tri-FBM', plot_path=plot_path)
+                                                series_name=f'{self.tri_factor} Tri-FBM', plot_path=plot_path,
+                                                y_limits=y_limits)
         return tri_fbm
 
 
@@ -210,9 +256,9 @@ class DprwFbmSimulator(DprwBiFbmSimulator):
         super().__init__(sample_size=sample_size, hurst_parameter=hurst_parameter, bi_factor=1,
                          lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def get_fbm(self, is_plot=False, seed=None, plot_path: str = None):
+    def get_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
         fbm = self.get_self_similar_process(is_plot=is_plot, seed=seed, method_name='DPRW', series_name='FBM',
-                                            plot_path=plot_path)
+                                            plot_path=plot_path, y_limits=y_limits)
         return fbm
 
 
@@ -242,20 +288,23 @@ class DprwNegFbmSimulator(DprwSelfSimilarFractalSimulator):
                          covariance_func=self.neg_fbm_covariance_func,
                          lamperti_multiplier=lamperti_multiplier, tmax=tmax, std_const=std_const)
 
-    def neg_fbm_covariance_func(self, k):
-        n = self.sample_size
-        h = self.hurst_parameter
+    def neg_fbm_covariance_func(self, k_de, n_de, hurst_de, factor_de):
+        n = n_de
+        h = hurst_de
         h2 = 2 * h
-        numerator = (scipy.special.gamma(h + 0.5)) ** 2 * (n ** (h * (k / n)) + n ** (h * (-k / n)) - (
-                (np.abs(1 - n ** (-k / n))) ** h) * (np.abs(1 - n ** (k / n))) ** h)
-        denominator = 2 * scipy.special.gamma(h2 + 1) * np.sin(h * np.pi)
-        term = -n ** (-np.abs(k / n) / 2) / (h + 0.5)
-        summ = scipy.special.hyp2f1(0.5 - h, 1, h + 3 / 2, n ** (-np.abs(k / n)))
-        v = numerator / denominator + term * summ
-        v[0] = (scipy.special.gamma(h + 0.5) ** 2) / (scipy.special.gamma(h2 + 1) * np.sin(h * np.pi)) - 1 / h2
+        k = k_de
+        if k_de == mpf('0'):
+            v = (gamma(h + 0.5) ** 2) / (gamma(h2 + 1) * sin(h * mp.pi)) - 1 / h2
+        else:
+            numerator = (gamma(h + 0.5)) ** 2 * (n ** (h * (k / n)) + n ** (h * (-k / n)) - (
+                    (abs(1 - n ** (-k / n))) ** h) * (abs(1 - n ** (k / n))) ** h)
+            denominator = 2 * gamma(h2 + 1) * sin(h * mp.pi)
+            term = -n ** (-abs(k / n) / 2) / (h + 0.5)
+            summ = hyp2f1(0.5 - h, 1, h + 3 / 2, n ** (-abs(k / n)))
+            v = numerator / denominator + term * summ
         return v
 
-    def get_neg_fbm(self, is_plot=False, seed=None, plot_path: str = None):
+    def get_neg_fbm(self, is_plot=False, seed=None, plot_path: str = None, y_limits: list = None):
         neg_fbm = self.get_self_similar_process(is_plot=is_plot, seed=seed, method_name='DPRW', series_name='Neg-FBM',
-                                                plot_path=plot_path)
+                                                plot_path=plot_path, y_limits=y_limits)
         return neg_fbm

fractal_analysis/simulator/dprw/dprw_multi_fractal_simulator.py
@@ -120,7 +120,8 @@ class DprwMbmSimulator(MultiFractalBaseSimulator):
         pos = np.sum(M, axis=1)
         return pos
 
-    def get_mbm(self, is_plot: bool = False, seed: int = None, hurst_name: str = '', plot_path: str = None):
+    def get_mbm(self, is_plot: bool = False, seed: int = None, hurst_name: str = '', plot_path: str = None,
+                y_limits: list = None):
         means, labels = self.get_kmeans()
         # precalculation of 1D fBm in the length of k with random inputs.
         base_fbm = self.get_fractal_bases(means=means, seed=seed)
@@ -134,5 +135,6 @@ class DprwMbmSimulator(MultiFractalBaseSimulator):
                 1 / 2) / scipy.special.gamma(self.holder_exponents + 1 / 2)) * (neg_mbm + pos_mbm)
         mbm[-1] = mbm[-2]
         if is_plot:
-            self.plot(series=mbm, method_name='DPRW', series_name='MBM', hurst_name=hurst_name, save_path=plot_path)
+            self.plot(series=mbm, method_name='DPRW', series_name='MBM', hurst_name=hurst_name, save_path=plot_path,
+                      y_limits=y_limits)
         return mbm